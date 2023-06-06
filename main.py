d71ea30e84ab42c9e6f383df3b4b1f9f27bcacee
sm0624 <91932859+sm0624@users.noreply.github.com>
2023-06-06 15:40:13 +0000

    deleted unnecessary comments

--- main.py
+++ main.py
@@ -5,11 +5,7 @@ def trace(*args):
   #print (*args)  # Comment out this line to remove debug output
   pass
 
-# This base URL works, but doesn't return anything too interesting
-# After running this script, read https://www.boredapi.com/ and
-# figure out how to change it to get back a filtered activity.
-# The filter is up to you: number of people, category, price, etc.
-# Tip: try testing the API URLs directly in a browser first
+
 URL = "https://swapi.dev/api/people/?"
 
 # Get data from the web site and put it into Python collections
@@ -44,19 +40,16 @@ question =input("Do you want to learn about more characters?")
 if input == "yes":
   URL = "https://swapi.dev/api/people/?page=2"
   trace ("Calling", URL)
-response = requests.get(URL) # Get data from the URL
-response.raise_for_status()  # Throw an exception if the request failed
-data = response.json()       # Parse the response into JSON
-
-# See what the raw data looks like
-trace ("\nText returned:", response.text)
-
-# You can also loop through each item (name/value pairs) in the JSON
-trace ("\nHere are all the kay/value pairs in the JSON response:")
+  response = requests.get(URL) # Get data from the URL
+  response.raise_for_status()  # Throw an exception if the request failed
+  data = response.json()       # Parse the response into JSON
+  trace ("\nText returned:", response.text)
+  
+  trace ("\nHere are all the kay/value pairs in the JSON response:")
 for key, value in data.items():
   trace (key, ": ", value)
 
-people = data["results"]
+people = data["results"] 
 for person in people:
   print (person["name"]) 
 useranswer = input("Which person do you want to learn more about?")
@@ -68,8 +61,7 @@ for useranswer in people:
   print (useranswer["eye_color"])     
   print (useranswer["birth_year"])
   break
-elif input == "no": 
- break
+
 
 
 name = input("Which person do you want to learn more about? ").lower()
